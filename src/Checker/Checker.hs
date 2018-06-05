e516a39f18784aadd7b01a53cab0536132de8805
keuhdall <lmarques@student.42.fr>
2018-06-06 00:52:02 +0200

    started working on canReachEnd function

--- src/Checker/Checker.hs
+++ src/Checker/Checker.hs
@@ -1,4 +1,4 @@
-module Checker.Checker (isValidMachine, isValidInput) where
+module Checker.Checker (isValidMachine, isValidInput, canReachEnd) where
     import Prelude hiding (read)
     import Data.Map
     import Machine.Data
@@ -7,9 +7,31 @@ module Checker.Checker (isValidMachine, isValidInput) where
 
     isValidMachine :: Maybe Machine -> Maybe Machine
     isValidMachine machine = case machine of
-        Just machine    -> checkMachine machine
+        Just machine    -> if (canReachEnd machine) then checkMachine machine else Nothing
         Nothing         -> Nothing
         where
+            canReachEnd :: Machine -> Bool
+            canReachEnd m = 
+                canReachEnd' m (toList (transitions m))                    
+                where
+                    canReachEnd' :: Machine -> [(String, [ActionTransition])] -> Bool
+                    canReachEnd' m transitionsList = case (transitionsList m) of
+                        (hd:tl) ->
+                            if (parseActionTransitionList (snd hd)) then
+                                True
+                            else
+                                canReachEnd' m tl
+                        []      -> False
+
+                    parseActionTransitionList :: Machine -> [ActionTransition] -> Bool
+                    parseActionTransitionList m atList = case atList of
+                        (hd:tl) ->
+                            if ((action hd) `member` (finals m)) then
+                                True
+                            else
+                                parseActionTransitionList m tl
+                        []      -> False
+
             checkMachine :: Machine -> Maybe Machine
             checkMachine machine =
                 if (checkTransitions machine (states machine)) then
@@ -40,7 +62,7 @@ module Checker.Checker (isValidMachine, isValidInput) where
                               False
                       []      -> True
 
-    
+
     isValidInput :: String -> [String] -> String -> Maybe String
     isValidInput input alphabet blank =
         if isValidInput' input alphabet blank then Just input else Nothing
