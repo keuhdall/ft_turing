6441e7a645977ef4506c17b022b7b1aa6b2b6cf5
Anis GADHGADHI <agadhgad@e3r12p23.42.fr>
2018-06-08 15:48:55 +0200

    add usage

--- src/main.hs
+++ src/main.hs
@@ -10,11 +10,29 @@ import Checker.Checker
 import qualified Data.ByteString.Lazy as B
 fget filename = B.readFile filename
 
+usage :: IO ()
+usage = do
+  name <- getProgName
+  putStrLn $ "usage: " ++ (show name) ++ " [-h] jsonfile input"
+  putStrLn $ ""
+  putStrLn $ "positional arguments:"
+  putStrLn $ "  jsonfile              json description of the machine"
+  putStrLn $ ""
+  putStrLn $ "  input                 input of the machine"
+  putStrLn $ ""
+  putStrLn $ "optional arguments:"
+  putStrLn $ "  -h, --help            show this help message and exit"
+
 main :: IO ()
 main = do
   args <- getArgs
+  if ((length args) >= 1) && (((args !! 0) == "-h") || ((args !! 0) == "--help")) then
+     usage
+  else do
+  if not ((length args) == 2) then
+    putStrLn "missing parameters"
+  else do
   let input = (args !! 1)
-  putStrLn "parsing the json..."
   s <- fget (args !! 0)
   case isValidMachine (decode s :: Maybe Machine) of
       Nothing       -> putStrLn "machine invalid"
