7a3bc6e5bd0de2136553e49f259aab5655a7bf68
keuhdall <lmarques@student.42.fr>
2018-06-09 18:03:59 +0200

    moved usage function to Logger module

--- src/main.hs
+++ src/main.hs
@@ -10,24 +10,11 @@ import Checker.Checker
 import qualified Data.ByteString.Lazy as B
 fget filename = B.readFile filename
 
-usage :: IO ()
-usage = do
-  name <- getProgName
-  putStrLn $ "usage: " ++ (show name) ++ " [-h] jsonfile input"
-  putStrLn $ ""
-  putStrLn $ "positional arguments:"
-  putStrLn $ "  jsonfile              json description of the machine"
-  putStrLn $ ""
-  putStrLn $ "  input                 input of the machine"
-  putStrLn $ ""
-  putStrLn $ "optional arguments:"
-  putStrLn $ "  -h, --help            show this help message and exit"
-
 main :: IO ()
 main = do
   args <- getArgs
   if ((length args) >= 1) && (((args !! 0) == "-h") || ((args !! 0) == "--help")) then
-     usage
+     printUsage
   else do
   if not ((length args) == 2) then
     putStrLn "missing parameters"
