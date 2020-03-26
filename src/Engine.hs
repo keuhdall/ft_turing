186f46ceaf373975838745ffbca6dd767043fbe0
Léonard Marques <lmarques@student.42.fr>
2020-03-26 01:39:32 +0100

    kinda improved printing of steps, but still not great as final state is still missing

--- src/Engine.hs
+++ src/Engine.hs
@@ -10,8 +10,8 @@ import Data.List.Split (chunksOf)
 import Types
 import Logger
 
-initialState :: Machine -> String -> Engine
-initialState Machine{initial} s = Engine {
+setInitialState :: Machine -> String -> Engine
+setInitialState Machine{initial} s = Engine {
   step    = 0,
   pos     = 0,
   initpos = 0,
@@ -25,14 +25,29 @@ initialState Machine{initial} s = Engine {
 currentWord :: Engine -> Maybe String
 currentWord Engine{pos,tape} = tape !? pos
 
+displayInitialState :: Engine -> Machine -> IO ()
+displayInitialState e m = case currentWord e >>= extractTransition e m of
+  Just t  -> printStep e m t
+  _       -> pure ()
+
+extractTransition :: Engine -> Machine -> String -> Maybe Transition
+extractTransition Engine{state} Machine{transitions} w = t >>= \t' -> findTransition (-1) $ zip t' [0..] where
+  t = lookup state transitions
+  findTransition :: Int -> [(Transition, Int)] -> Maybe Transition
+  findTransition pos t' = case t' of
+    (a,b):xs  -> if read a == w then Just a else findTransition (if pos == -1 && read a == "ANY" then b else pos) xs
+    []        -> if pos == -1 then Nothing else t >>= \t'' -> Just (t'' !! pos) {read = w}
+
 run :: Machine -> String -> IO ()
 run m@Machine{..} input = do
+  let initState = setInitialState m input
   printHeader m
-  next $ initialState m input
+  displayInitialState initState m
+  next initState
   where
     next :: Engine -> IO ()
-    next e = case currentWord e >>= extractTransition e >>= apply e of
-      Just (e', t) -> printStep e m t >> next e'
+    next e = case currentWord e >>= extractTransition e m >>= apply e of
+      Just (e', t) -> printStep e' m t >> next e'
       Nothing -> putStrLn "program finished"
 
     apply :: Engine -> Transition -> Maybe (Engine,Transition)
@@ -51,11 +66,3 @@ run m@Machine{..} input = do
     replace Engine{tape,pos} w newpos = checkTape <$> zip filledTape [0..] where
       checkTape (a,b) = if b == pos then w else a
       filledTape = let x = abs $ newpos + 1 in if newpos < 0 then ([0..x] >> blank):tape else tape
-
-    extractTransition :: Engine -> String -> Maybe Transition
-    extractTransition Engine{state} w = t >>= \t' -> findTransition (-1) $ zip t' [0..] where
-      t = lookup state transitions
-      findTransition :: Int -> [(Transition, Int)] -> Maybe Transition
-      findTransition pos t' = case t' of
-        (a,b):xs  -> if read a == w then Just a else findTransition (if pos == -1 && read a == "ANY" then b else pos) xs
-        []        -> if pos == -1 then Nothing else t >>= \t'' -> Just (t'' !! pos) {read = w}
